17525f6bc01948e26d4291d9c97a889233aff77a
Arunava Modak <arunavamodak@Arunavas-MacBook-Pro.local>
2022-01-14 23:23:36 +0530

    404 page

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -4,9 +4,10 @@ import AllBanks from "./AllBanks";
 import BankDetails from "./BankDetails";
 import Favourites from "./Favourites";
 import Structure from "../Components/Structure";
+import NotFound from "./NotFound";
 
 function Index() {
-    
+
     const WrappedComponent = (component) =>{
         return(
             <Structure component={component}></Structure>
@@ -20,6 +21,7 @@ function Index() {
                 <Route path="/all-banks" element={WrappedComponent(<AllBanks/>)} />
                 <Route path="/favourites" element={WrappedComponent(<Favourites/>)} />
                 <Route path="/bank-details/:id" element={WrappedComponent(<BankDetails/>)} />
+                <Route path="*" element={<NotFound/>} />
             </Routes>
         </BrowserRouter>
     )
